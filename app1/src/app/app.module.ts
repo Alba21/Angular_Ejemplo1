ac51ba44241ef608c21250aada1e77bfb6eae7d3
Albita21 <300290527@ingenieria.usac.edu.gt>
2019-09-10 09:52:21 -0600

    Auto stash before rebase of "origin/master"

--- app1/src/app/app.module.ts
+++ app1/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { Componente1Component } from './componente1/componente1.component';
@@ -15,8 +14,7 @@ import { TablaComponent } from './tabla/tabla.component';
   ],
   imports: [
     BrowserModule,
-    NgbModule,
-    FormsModule
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- app1/src/app/componente1/componente1.component.ts
+++ app1/src/app/componente1/componente1.component.ts
@@ -6,11 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./componente1.component.css']
 })
 export class Componente1Component implements OnInit {
+<<<<<<< HEAD
   public correo : string;
   public password : string;
   public saludar(){
     
   }
+=======
+
+>>>>>>> parent of fd5e767... ngModule
   constructor() { }
 
   ngOnInit() {
